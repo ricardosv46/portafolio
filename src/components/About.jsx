c5f96a24deed39af6c10809f49adcd94e261b04d
ricardo <ricardosv46@gmail.com>
2024-04-13 10:10:12 -0500

    update cv and aboout me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,38 @@
-import React from "react"
+import React from 'react'
 
 const About = () => {
   return (
-    <section className='about section bd-container' id='about'>
-      <span className='section-subtitle'>Sobre mi</span>
+    <section className="about section bd-container" id="about">
+      <span className="section-subtitle">Sobre mi</span>
 
-      <h2 className='section-title'>Ricardo Solis</h2>
+      <h2 className="section-title">Ricardo Solis</h2>
 
-      <div className='about__container bd-grid'>
-        <p className='about__description'>
-        Soy un desarrollador Full Stack apasionado por crear experiencias web elegantes y eficientes. Tengo experiencia en el desarrollo de sitios web y aplicaciones web utilizando HTML, CSS y JavaScript. Además, utilizo tecnologías como React, Next.js, TypeScript y TailwindCSS para crear aplicaciones web escalables y personalizables con una experiencia de usuario fluida.<br/><br/>
-
-        En cuanto al desarrollo de backend, tengo habilidades en Express, Node.js y bases de datos NoSQL como MongoDB. También cuento con experiencia en autenticación de usuarios utilizando JSON Web Tokens (JWT) y en la gestión de estados utilizando Redux. Además, tengo habilidades en el consumo de APIs y GraphQL, lo que me permite obtener información desde el backend de manera efectiva y eficiente.<br/><br/>
-
-        Mantengo mis proyectos organizados y actualizados mediante el uso de Git y GitHub. Estoy comprometido a mantenerme al día con las últimas tendencias y tecnologías en el desarrollo web. Disfruto trabajando en equipo para crear soluciones web de alta calidad.<br/><br/>
-
-        Si necesitas ayuda en el desarrollo de una aplicación web o móvil completa, desde el frontend hasta el backend, ¡no dudes en contactarme!
+      <div className="about__container bd-grid">
+        <p className="about__description">
+          {/* Soy un desarrollador Full Stack apasionado por crear experiencias web elegantes y eficientes. Tengo experiencia en el desarrollo
+          de sitios web y aplicaciones web utilizando HTML, CSS y JavaScript. Además, utilizo tecnologías como React, Next.js, TypeScript y
+          TailwindCSS para crear aplicaciones web escalables y personalizables con una experiencia de usuario fluida.
+          <br />
+          <br />
+          En cuanto al desarrollo de backend, tengo habilidades en Express, Node.js y bases de datos NoSQL como MongoDB. También cuento con
+          experiencia en autenticación de usuarios utilizando JSON Web Tokens (JWT) y en la gestión de estados utilizando Redux. Además,
+          tengo habilidades en el consumo de APIs y GraphQL, lo que me permite obtener información desde el backend de manera efectiva y
+          eficiente.
+          <br />
+          <br />
+          Mantengo mis proyectos organizados y actualizados mediante el uso de Git y GitHub. Estoy comprometido a mantenerme al día con las
+          últimas tendencias y tecnologías en el desarrollo web. Disfruto trabajando en equipo para crear soluciones web de alta calidad.
+          <br />
+          <br /> */}
+          {/* Si necesitas ayuda en el desarrollo de una aplicación web o móvil completa, desde el frontend hasta el backend, ¡no dudes en
+          contactarme!  */}
+          Desarrollador web con 2 años de experiencia en la creación de soluciones de T.I. Me he desempeñado en proyectos de software
+          especializado en tecnologías web, abarcando tanto el desarrollo frontend como el backend, para diversos sectores como
+          telecomunicaciones, tecnología, seguros, público, entre otros. <br />
+          <br />
+          Mi objetivo es desarrollar soluciones tecnológicas completas y ofrecer una experiencia digital excepcional a los usuarios. Estoy
+          comprometido con mantenerme actualizado en las últimas tendencias y tecnologías, y disfruto trabajando en equipo para lograr
+          resultados sobresalientes.
         </p>
       </div>
     </section>
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,79 +1,74 @@
-import React from "react"
-import Pdf from "../assets/pdf/cv-ricardo-solis.pdf"
-import { motion } from "framer-motion"
+import React from 'react'
+import Pdf from '../assets/pdf/cv-ricardo-2024.pdf'
+import { motion } from 'framer-motion'
 
 const Hello = () => {
   return (
-    <section className='home' id='home'>
-      <div className='home__container bd-container bd-grid'>
+    <section className="home" id="home">
+      <div className="home__container bd-container bd-grid">
         <motion.div
-          className='home__data'
+          className="home__data"
           initial={{ y: 25, opacity: 0 }}
           transition={{
             duration: 0.5,
-            delay: 0,
+            delay: 0
           }}
           animate={{
             y: 0,
-            opacity: 1,
-          }}
-        >
+            opacity: 1
+          }}>
           <motion.span
-            className='home__greeting'
+            className="home__greeting"
             initial={{ y: 25, opacity: 0 }}
             transition={{
               duration: 0.5,
-              delay: 0,
+              delay: 0
             }}
             animate={{
               y: 0,
-              opacity: 1,
-            }}
-          >
+              opacity: 1
+            }}>
             Hello, My name is
           </motion.span>
           <motion.h1
-            className='home__name'
+            className="home__name"
             initial={{ y: 25, opacity: 0 }}
             transition={{
               duration: 0.7,
-              delay: 0.2,
+              delay: 0.2
             }}
             animate={{
               y: 0,
-              opacity: 1,
-            }}
-          >
+              opacity: 1
+            }}>
             Ricardo Solis
           </motion.h1>
           <motion.span
-            className='home__profession'
+            className="home__profession"
             initial={{ y: 25, opacity: 0 }}
             transition={{
               duration: 0.9,
-              delay: 0.4,
+              delay: 0.4
             }}
             animate={{
               y: 0,
-              opacity: 1,
-            }}
-          >
+              opacity: 1
+            }}>
             Web Developer
           </motion.span>
           <motion.a
             initial={{ y: 25, opacity: 0 }}
             transition={{
               duration: 0.9,
-              delay: 0.4,
+              delay: 0.4
             }}
             animate={{
               y: 0,
-              opacity: 1,
+              opacity: 1
             }}
             download="cv ricardo.pdf"
             href={Pdf}
-            className='button button-light home__button'
-          >
+            className="button button-light home__button">
             Download CV
           </motion.a>
         </motion.div>
